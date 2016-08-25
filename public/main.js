5c99f1042d971d56b69ae1d9e63e6431cd091a3b
Dong Qihong <dongqihong@vip.qq.com>
2016-08-25 14:44:12 +0800

    添加ECharts绘图

--- public/main.js
+++ public/main.js
@@ -1,32 +1,90 @@
-var socket = io();
+$(function () {
+    var socket = io();
 
-function motion(event) {
-    document.getElementById('accelerometer').innerHTML = 'Accelerometer: '
-        + event.accelerationIncludingGravity.x + ', '
-        + event.accelerationIncludingGravity.y + ', '
-        + event.accelerationIncludingGravity.z;
-
-    socket.emit('motion', {
-        x: event.accelerationIncludingGravity.x,
-        y: event.accelerationIncludingGravity.y,
-        z: event.accelerationIncludingGravity.z
+    var motionChart = echarts.init(document.getElementById('accelerometer'));
+    motionChart.setOption({
+        title: {
+            text: '加速度传感器'
+        },
+        tooltip: {},
+        legend: {
+            data: ['加速度']
+        },
+        xAxis: {
+            data: []
+        },
+        yAxis: {},
+        series: [{
+            name: '加速度',
+            type: 'bar',
+            data: []
+        }]
     });
-}
 
-function orientation(event) {
-    document.getElementById('magnetometer').innerHTML = 'Magnetometer: '
-        + event.alpha + ', '
-        + event.beta + ', '
-        + event.gamma;
-
-    socket.emit('orientation', {
-        alpha: event.alpha,
-        beta: event.beta,
-        gamma: event.gamma
+    var orientationChart = echarts.init(document.getElementById('magnetometer'));
+    orientationChart.setOption({
+        title: {
+            text: '磁力计传感器'
+        },
+        tooltip: {},
+        legend: {
+            data: ['方位角度']
+        },
+        xAxis: {
+            data: []
+        },
+        yAxis: {},
+        series: [{
+            name: '方位角度',
+            type: 'bar',
+            data: []
+        }]
     });
-}
 
-function go() {
+    function motion(event) {
+        motionChart.setOption({
+            xAxis: {
+                data: ['x', 'y', 'z']
+            },
+            series: [{
+                name: '加速度',
+                data: [
+                    event.accelerationIncludingGravity.x,
+                    event.accelerationIncludingGravity.y,
+                    event.accelerationIncludingGravity.z
+                ]
+            }]
+        });
+
+        socket.emit('motion', {
+            x: event.accelerationIncludingGravity.x,
+            y: event.accelerationIncludingGravity.y,
+            z: event.accelerationIncludingGravity.z
+        });
+    }
+
+    function orientation(event) {
+        orientationChart.setOption({
+            xAxis: {
+                data: ['alpha', 'beta', 'gamma']
+            },
+            series: [{
+                name: '方位角度',
+                data: [
+                    event.alpha,
+                    event.beta,
+                    event.gamma
+                ]
+            }]
+        });
+
+        socket.emit('orientation', {
+            alpha: event.alpha,
+            beta: event.beta,
+            gamma: event.gamma
+        });
+    }
+
     if (window.DeviceMotionEvent) {
         window.addEventListener('devicemotion', motion, false);
     } else {
@@ -40,6 +98,4 @@ function go() {
         var status = document.getElementById('status');
         status.innerHTML = status.innerHTML.replace('is supported', 'is not supported');
     }
-}
-
-window.onload = go;
+});
