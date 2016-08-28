4d4f2828a033cd87baf3d4542f0b4c827db003da
Dong Qihong <dongqihong@vip.qq.com>
2016-08-28 16:20:31 +0800

    添加npm脚本命令

--- app.js
+++ app.js
@@ -10,19 +10,3 @@ server.listen(port, function () {
 });
 
 app.use(express.static(__dirname + '/public'));
-
-// io.on('connection', function (socket) {
-//     socket.emit('uuid', {uuid: uuid.v1()});
-//
-//     socket.on('motion', function (motion) {
-//         console.log('uuid: %s - x: %d, y: %d, z: %d', motion.uuid, motion.x, motion.y, motion.z);
-//     });
-//
-//     socket.on('orientation', function (orientation) {
-//         console.log('uuid: %s - alpha: %d, beta: %d, gamma: %d',
-//             orientation.uuid,
-//             orientation.alpha,
-//             orientation.beta,
-//             orientation.gamma);
-//     });
-// });
