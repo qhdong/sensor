fe239a7be5106765bd98474017af158440164ad5
Dong Qihong <dongqihong@vip.qq.com>
2016-09-01 19:47:49 +0800

    删除图标绘制

--- public/main.js
+++ public/main.js
@@ -17,106 +17,106 @@ $(function () {
     });
 
 
-    var motionChart = echarts.init(document.getElementById('accelerometer'));
-    motionChart.setOption({
-        title: {
-            text: '加速度传感器'
-        },
-        tooltip: {},
-        legend: {
-            data: ['x', 'y', 'z']
-        },
-        xAxis: {
-            type: 'time',
-            splitLine: {
-                show: false
-            }
-        },
-        yAxis: {
-            type: 'value',
-            splitLine: {
-                show: false
-            }
-        },
-        series: [
-            {
-                name: 'x',
-                type: 'line',
-                data: []
-            }, {
-                name: 'y',
-                type: 'line',
-                data: []
-            }, {
-                name: 'z',
-                type: 'line',
-                data: []
-            }
-        ]
-    });
-
-    var orientationChart = echarts.init(document.getElementById('magnetometer'));
-    orientationChart.setOption({
-        title: {
-            text: '磁力计传感器'
-        },
-        tooltip: {},
-        legend: {
-            data: ['alpha', 'gamma', 'beta']
-        },
-        xAxis: {
-            type: 'time',
-            splitLine: {
-                show: false
-            }
-        },
-        yAxis: {
-            type: 'value',
-            splitLine: {
-                show: false
-            }
-        },
-        series: [
-            {
-                name: 'alpha',
-                type: 'line',
-                data: []
-            }, {
-                name: 'beta',
-                type: 'line',
-                data: []
-            }, {
-                name: 'gamma',
-                type: 'line',
-                data: []
-            }
-        ]
-    });
+    // var motionChart = echarts.init(document.getElementById('accelerometer'));
+    // motionChart.setOption({
+    //     title: {
+    //         text: '加速度传感器'
+    //     },
+    //     tooltip: {},
+    //     legend: {
+    //         data: ['x', 'y', 'z']
+    //     },
+    //     xAxis: {
+    //         type: 'time',
+    //         splitLine: {
+    //             show: false
+    //         }
+    //     },
+    //     yAxis: {
+    //         type: 'value',
+    //         splitLine: {
+    //             show: false
+    //         }
+    //     },
+    //     series: [
+    //         {
+    //             name: 'x',
+    //             type: 'line',
+    //             data: []
+    //         }, {
+    //             name: 'y',
+    //             type: 'line',
+    //             data: []
+    //         }, {
+    //             name: 'z',
+    //             type: 'line',
+    //             data: []
+    //         }
+    //     ]
+    // });
+    //
+    // var orientationChart = echarts.init(document.getElementById('magnetometer'));
+    // orientationChart.setOption({
+    //     title: {
+    //         text: '磁力计传感器'
+    //     },
+    //     tooltip: {},
+    //     legend: {
+    //         data: ['alpha', 'gamma', 'beta']
+    //     },
+    //     xAxis: {
+    //         type: 'time',
+    //         splitLine: {
+    //             show: false
+    //         }
+    //     },
+    //     yAxis: {
+    //         type: 'value',
+    //         splitLine: {
+    //             show: false
+    //         }
+    //     },
+    //     series: [
+    //         {
+    //             name: 'alpha',
+    //             type: 'line',
+    //             data: []
+    //         }, {
+    //             name: 'beta',
+    //             type: 'line',
+    //             data: []
+    //         }, {
+    //             name: 'gamma',
+    //             type: 'line',
+    //             data: []
+    //         }
+    //     ]
+    // });
 
     function motion(event) {
-        if (xData.length > POINTS) {
-            xData.shift();
-            yData.shift();
-            zData.shift();
-        }
+        // if (xData.length > POINTS) {
+        //     xData.shift();
+        //     yData.shift();
+        //     zData.shift();
+        // }
+        //
+        // xData.push(event.accelerationIncludingGravity.x);
+        // yData.push(event.accelerationIncludingGravity.y);
+        // zData.push(event.accelerationIncludingGravity.z);
 
-        xData.push(event.accelerationIncludingGravity.x);
-        yData.push(event.accelerationIncludingGravity.y);
-        zData.push(event.accelerationIncludingGravity.z);
-
-        motionChart.setOption({
-            series: [{
-                name: 'x',
-                data: xData
-            }, {
-                name: 'y',
-                data: yData
-            }, {
-                name: 'z',
-                data: zData
-            }
-            ]
-        });
+        // motionChart.setOption({
+        //     series: [{
+        //         name: 'x',
+        //         data: xData
+        //     }, {
+        //         name: 'y',
+        //         data: yData
+        //     }, {
+        //         name: 'z',
+        //         data: zData
+        //     }
+        //     ]
+        // });
 
         socket.emit('motion', {
             uuid: uuid,
@@ -127,28 +127,28 @@ $(function () {
     }
 
     function orientation(event) {
-        if (alphaData.length > POINTS) {
-            alphaData.shift();
-            betaData.shift();
-            gammaData.shift();
-        }
-
-        alphaData.push(event.alpha);
-        betaData.push(event.beta);
-        gammaData.push(event.gamma);
-
-        orientationChart.setOption({
-            series: [{
-                name: 'alpha',
-                data: alphaData
-            }, {
-                name: 'beta',
-                data: betaData
-            }, {
-                name: 'gamma',
-                data: gammaData
-            }]
-        });
+        // if (alphaData.length > POINTS) {
+        //     alphaData.shift();
+        //     betaData.shift();
+        //     gammaData.shift();
+        // }
+        //
+        // alphaData.push(event.alpha);
+        // betaData.push(event.beta);
+        // gammaData.push(event.gamma);
+        //
+        // orientationChart.setOption({
+        //     series: [{
+        //         name: 'alpha',
+        //         data: alphaData
+        //     }, {
+        //         name: 'beta',
+        //         data: betaData
+        //     }, {
+        //         name: 'gamma',
+        //         data: gammaData
+        //     }]
+        // });
 
         socket.emit('orientation', {
             uuid: uuid,
